213f62c99ec1340142a7d0a9fb0e53d3c958b301
Khac Kien <ii.khackien@gmail.com>
2024-12-18 09:53:40 +0700

    Add content, update README

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -6,9 +6,16 @@ const Home = () => {
       <div className='container-xl'>
         <div className='flex'>
           <div
-            className='h-[320px] w-[540px]'
+            className='h-[320px] w-[540px] flex justify-center items-center'
             style={{ background: `url(${require('assets/images/meal_d_01.jpg')}) no-repeat center / cover` }}
-          />
+          >
+            <div className='w-[180px] h-[180px] rounded-full border-[3px] border-white border-t-transparent flex justify-center items-center -rotate-45 shadow-md'>
+              <div className='rotate-45'>
+                <span className='text-[18px] shadow-md mr-1'>05/21</span>
+                <span className='text-[24px] shadow-md'>75%</span>
+              </div>
+            </div>
+          </div>
           <div className='flex-1'>
             <BannerGraph />
           </div>
